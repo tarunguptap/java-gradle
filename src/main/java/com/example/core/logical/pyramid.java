630c581fc80c752739b9535b321be6aba2813c84
tarun <tarunguptap@yahoo.com>
2016-11-17 21:00:39 +0530

    Adding comment in pyramid

--- src/main/java/com/example/core/logical/pyramid.java
+++ src/main/java/com/example/core/logical/pyramid.java
@@ -2,7 +2,7 @@ package com.example.core.logical;
 
 public class pyramid {
 	
-	
+	//http://www.c-sharpcorner.com/blogs/creating-pyramid-in-c-sharp1
 	private static void asteriskPyramid(int numberOfLayer) {
 		
 	}
