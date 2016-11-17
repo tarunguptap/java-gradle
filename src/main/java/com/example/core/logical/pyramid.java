c4547130e2c1a4211f2a783f455f1892385aa6cb
tarun-gupta <tarunguptap@yahoo.com>
2016-11-17 13:19:29 +0530

    Pyramid

--- src/main/java/com/example/core/logical/pyramid.java
+++ src/main/java/com/example/core/logical/pyramid.java
@@ -1,9 +1,26 @@
 package com.example.core.logical;
 
 public class pyramid {
-	
-	//http://www.c-sharpcorner.com/blogs/creating-pyramid-in-c-sharp1
-	private static void asteriskPyramid(int numberOfLayer) {
-		
-	}
+
+    public static void main(String[] args) {
+        asteriskPyramid(6);
+    }
+
+    // http://www.c-sharpcorner.com/blogs/creating-pyramid-in-c-sharp1
+    private static void asteriskPyramid(int numberOfLayer) {
+        int numberoflayer = 6, Space, Number;
+        System.out.println("Print paramid");
+        for (int i = 1; i <= numberoflayer; i++) {// Total number of layer for pramid
+            for (Space = 1; Space <= (numberoflayer - i); Space++)
+                // Loop For Space
+                System.out.print(" ");
+            for (Number = 1; Number <= i; Number++)
+                // increase the value
+                System.out.print("*");
+            for (Number = (i - 1); Number >= 1; Number--)
+                // decrease the value
+                System.out.print("*");
+            System.out.println();
+        }
+    }
 }
